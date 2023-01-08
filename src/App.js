8cfc3d836de213db2f639cc7708fd5668dccb50c
Adarsh Tripathi <adarsh@probo.in>
2023-01-08 20:01:51 +0530

    form controlled using use ref hook

--- src/App.js
+++ src/App.js
@@ -1,93 +1,84 @@
 import './App.css';
-import React from 'react';
+import React,{useRef} from 'react';
 
 function App() {
-  const initialFormData = {
-    userName : "",
-    password : "",
-    fullName : "",
-    phone : "",
-    confirmPassword : ""
-  }
+  const userNameRef = useRef();
+  const nameRef = useRef();
+  const phoneRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
 
-  const [formData,setFormData] = React.useState(initialFormData);
-  const [isSame,setIsSame] = React.useState(false);
-  const [isPhoneValid,setPhoneValid] = React.useState(true);
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name] : e.target.value
-    })
-  }
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    if(formData.password !== formData.confirmPassword){
-      setIsSame(true);
-      return;
-    }
-    if (formData.phone !== "") {
+    const fullName = nameRef.current.value;
+    const userName = nameRef.current.value;
+    const phone = phoneRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+
+    if (phone !== "") {
       var pattern = new RegExp(/^[0-9\b]+$/);
     
-      if (!pattern.test(formData.phone) || formData.phone.length !== 10) {
-        setPhoneValid(false); 
+      if (!pattern.test(phone) || phone.length !== 10) {
+        alert('Phone no is not valid!');
         return;
       }
     }
-    setPhoneValid(true);
-    setIsSame(false);
-    console.log(JSON.stringify(formData));
+
+    if(password !== confirmPassword){
+      alert('Passwords do not match!');
+      return;
+    } 
+    
+    alert('A form was submitted with UserName: "' + userName + '", Name: "'+ fullName +'" and Phone: "' + phone + '"');
+    
   }
 
   return (
-    <div className="App">
-      <div className='formContainer' onSubmit={handleFormSubmit}> 
-        <form>
+    <div className='formContainer'> 
+      <form onSubmit={handleFormSubmit}>
           <div>
-            <input placeholder='User Name' 
-              onChange={handleChange} 
-              value={formData.userName}
-              name = "userName"
+            <input 
+              placeholder='User Name' 
+              type = "text"
+              ref={userNameRef}
+              required
             />
           </div>
           <div>
-            <input placeholder='Full Name' 
-              onChange={handleChange} 
-              value={formData.fullName}
-              name = "fullName"
+            <input 
+              placeholder='Full Name' 
+              type = "text"
+              ref={nameRef}
+              required
             />
           </div>
           <div>
-            <input placeholder='phone' 
-              onChange={handleChange} 
-              value={formData.phone}
-              name = "phone"
+            <input placeholder='Phone' 
+              type = "text"
+              ref={phoneRef}
+              required
             />
-            {!isPhoneValid && <p>Phone Number is not valid.</p>}
           </div>
           <div>
-            <input placeholder='password' 
-              onChange={handleChange} 
-              value={formData.password}
-              name="password"
-              type="password"
+            <input placeholder='Password' 
+              type = "password"
+              ref={passwordRef}
+              required
             />
           </div>
           <div>
             <input placeholder='Confirm Password' 
-              onChange={handleChange} 
-              value={formData.confirmPassword}
-              name="confirmPassword"
-              type="password"
+              type = "password"
+              ref={confirmPasswordRef}
+              required
             />
-            {isSame && <p>Passwords do not match</p>}
           </div>
           <div>
             <button>Submit</button>
           </div>
-        </form>
-      </div>
+      </form>
     </div>
   );
 }
